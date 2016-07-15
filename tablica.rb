7219f22e7e28d50a23c9f80509c79561cc80d08f
damianp63 <damianpodrazka@wp.pl>
2016-07-15 17:47:30 +0200

    sprawdzanie w dwóch wymiarach (problem z powtórzeniami w ostatnim miejscu wersu)

--- tablica.rb
+++ tablica.rb
@@ -1,39 +1,45 @@
 require 'matrix'
-#require 'attr_extras'
 
 class SquareUniqueMatrix
-#  vattr_initialize :table
-    def initialize(table)
-      @table=table
-    end
-    def CreateMatrixValue(table)
-      (0...table.size).each do |i|
-        (0...table.size).each do |j|
-          temporary_value=rand(1..table.size)
-          if(UniqeValues(table[i],temporary_value))
-            puts temporary_value
-            redo
-          else
-            table[i].push(temporary_value)
-          end
+  def initialize(table)
+    @table=table
+  end
+
+  def CreateMatrixValue(table)
+    (0...table.size).each do |i|
+      (0...table.size).each do |j|
+        temporary_value=rand(1..table.size)
+        print temporary_value
+        if(UniqeValues(table,i,j,temporary_value))
+          redo
+        else
+          table[i].push(temporary_value)
         end
       end
     end
+  end
 
-    def to_s(table)
-      table.each do |i|
-        i.each do |j|
-          print "#{j}\t"
-        end
-        print "\n"
+  def to_s(table)
+    table.each do |i|
+      i.each do |j|
+        print "#{j}\t"
       end
+      print "\n"
     end
+  end
 
-    def UniqeValues(table,value)
-        duplication_wight=table.any?{|element| element==value}
-      return duplication_wight
+  def UniqeValues(table,hight,wight,value)
+    temporary_table=[]
+    (0..hight).each do |i|
+      temporary_table.push(table[i][wight])
+    end
+    duplication_hight=temporary_table.any?{|element| element==value}
+    duplication_wight=table[hight].any?{|element| element==value}
+    print duplication_wight || duplication_hight
+    puts " "
+    return duplication_wight || duplication_hight
+  end
 
-end
 puts "podaj rozmiar macierzy"
 size = gets.chomp.to_i
 table=[]
