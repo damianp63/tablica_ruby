7473698c0ee28603aa8471fcfad6991c10e1cf65
damianp63 <damianpodrazka@wp.pl>
2016-07-21 12:14:50 +0200

    dodana metoda sprawdzająca powtórzenia

--- tablica.rb
+++ tablica.rb
@@ -3,7 +3,9 @@ require 'matrix'
 class SquareUniqueMatrix
   def initialize(table)
     @table=table
-    to_s(table)
+    if(valid?(table))
+      to_s(table)
+     end
   end
 
   def to_s(table)
@@ -15,5 +17,24 @@ class SquareUniqueMatrix
     end
   end
 
-SquareMatrix=SquareUniqueMatrix.new([[1,2,3],[3,1,2],[2,3,1]])
+  def valid?(table)
+    temporary_table=[]
+    duplication_table=[]
+    duplication_hight=duplication_wight=false
+    (0...table.size).each do |i|
+      (0...table.size).each do |j|
+        temporary_table.push(table[j][i])
+        if(temporary_table.detect{|e| temporary_table.count(e)>1})
+          duplication_hight=true
+        end
+        if(table[i].detect{|e| table[i].count(e)>1})
+          duplication_wight=true
+        end
+        duplication_table.push(duplication_wight||duplication_hight)
+      end
+     end
+      return duplication_table.any?{|element| element==true}
+  end
+
+ SquareMatrix=SquareUniqueMatrix.new([[1,2,3],[3,1,2],[2,3,1]])
 end
